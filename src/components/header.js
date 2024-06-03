709338513c5367eda93f6601fed2c6e0ba1ab0b4
LucaBla <me@lucablazevic.dev>
2024-06-03 20:12:40 +0200

    replace custom drawer with mui drawer

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,14 @@ import '../styles/components/header.css';
 import { useAuth } from "../authContext";
 import ProfileDrawer from "./profileDrawer";
 import { Link, useNavigate } from "react-router-dom";
+import { Box, Drawer } from "@mui/material";
+import { Logout, PersonOutline } from "@mui/icons-material";
+import { useState } from "react";
 
 function Header({name}) {
   const authToken = localStorage.getItem('authToken');
   const navigate = useNavigate();
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <div className="HeaderWrapper">
@@ -14,7 +18,31 @@ function Header({name}) {
         <h1>ScanVoucher</h1>
       </Link>
       {authToken?
-        <ProfileDrawer name={name}/>
+        <>
+          <PersonOutline onClick={()=>setDrawerOpen(true)}/>
+          <Drawer 
+            open={drawerOpen}
+            onClose={()=>setDrawerOpen(false)}
+            anchor="right"
+          >
+            <Box>
+              <div className="DrawerHeader">
+                <div className="LogoWrapper">
+                  <img src="/images/logo-placeholder-image.png"/>
+                </div>
+                <div className="InfoWrapper">
+                  {name}
+                </div>
+            </div>
+            <div className="DrawerContent">
+              <Link to={`/login`}>
+                <Logout/>
+                LogOut
+              </Link>
+            </div>
+            </Box>
+          </Drawer>
+        </>
         :
         <></>
       }
